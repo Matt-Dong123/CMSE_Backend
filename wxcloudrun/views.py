3caed63bc1bd7a19c7aa9eb65c36789fd7b0f9be
Matt <2274006799@qq.com>
2024-10-02 21:41:38 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -35,8 +35,11 @@ def counter(request, _):
         rsp = JsonResponse({'code': -1, 'errorMsg': '请求方式错误'},
                             json_dumps_params={'ensure_ascii': False})
     print(request)
-    logger.info(f'request: {request}')
-
+    logger.info(f'request: {request.META}')
+    logger.info(f'request: {request.META.get('X-WX-OPENID','OPENID')}')
+    logger.info(f'request: {request.META.get('X_WX_OPENID','_OPENID')}')
+    logger.info(f'request: {request.META.get('HTTP_X-WX-OPENID','H_OPENID')}')
+    logger.info(f'request: {request.META.get('HTTP_X_WX_OPENID','H1_OPENID')}')
     logger.info('response result: {}'.format(rsp.content.decode('utf-8')))
     return rsp
 
