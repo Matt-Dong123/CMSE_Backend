3fe98737de52e5a64f1d4c4641d6813ee2b9d61a
YunhaoDong <2274006799@qq.com>
2024-10-08 02:03:37 +0800

    cert

--- sysoptions/views.py
+++ sysoptions/views.py
@@ -2,16 +2,17 @@ import logging
 
 import requests
 from django.utils import timezone
-from rest_framework.exceptions import ValidationError
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from sysoptions.models import Files
-from sysoptions.serializers import FileUploadSerializer
-from wxcloudrun.settings import COS_BUCKET
+from wxcloudrun.settings import COS_BUCKET, CRET_PATH
 
 logger = logging.getLogger(__name__)
+
+
 # Create your views here.
 
 @api_view(("GET",))
@@ -30,14 +31,14 @@ def get_upload_url(request):
     if not path:
         raise ValidationError({"path": "path is required"})
 
-    url = "https://api.weixin.qq.com/tcb/uploadfile"
     payload = {
         "env": COS_BUCKET,
         "path": path
     }
+    url = f"https://api.weixin.qq.com/tcb/uploadfile"
     print(f"payload: {payload}")
     try:
-        response = requests.post(url, data=payload,verify=False)
+        response = requests.post(url, json=payload, verify=CRET_PATH)
         response.raise_for_status()
         response = response.json()
         print(f"response: {response}")
