9f303aeda43d3b146a35cb8dad9a62bcfeee4d31
YunhaoDong <2274006799@qq.com>
2024-10-15 01:18:58 +0800

    iso

--- activity/models.py
+++ activity/models.py
@@ -16,7 +16,7 @@ class ActivityType(IntegerChoices):
 
 
 def default_sign_info():
-    return dict(code="", valid_until=timezone.now())
+    return dict(code="", valid_until=timezone.now().isoformat())
 
 
 class Attender(models.Model):
@@ -51,6 +51,8 @@ class Activity(models.Model):
     location = models.CharField("地点", max_length=50)
     capacity = models.PositiveIntegerField("最大报名容量")
     type = models.IntegerField("活动类别", choices=ActivityType.choices)
+    # sign_code = models.CharField("签到码", max_length=50, null=True)
+    # code_expired_time = models.DateTimeField("签到码过期时间", null=True)
     sign_info = models.JSONField("签到信息", default=default_sign_info)
     users = models.ManyToManyField(
         User, through=Attender, related_name="activity_attenders_set", verbose_name="参与者"
