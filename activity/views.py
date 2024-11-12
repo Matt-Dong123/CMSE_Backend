e4c9f4b38fc814b36f1183a1a0a1fd31db4f9d00
YunhaoDong <2274006799@qq.com>
2024-11-13 00:16:29 +0800

    7200

--- activity/views.py
+++ activity/views.py
@@ -209,10 +209,10 @@ class ActivityManageViewSet(ModelViewSet):
         ttl = request.query_params.get("ttl", 10)
         try:
             ttl = int(ttl)
-            if not 5 <= ttl <= 30:
+            if not 5 <= ttl <= 7200:
                 raise ValueError
         except ValueError:
-            return Response({"message": "ttl需要为一个5~30之间的正整数"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message": "ttl需要为一个5~7200之间的正整数"}, status=status.HTTP_400_BAD_REQUEST)
         info = {
             "code": hashlib.md5(str(uuid1()).encode()).hexdigest(),
             "valid_until": to_django_time(timezone.now() + timezone.timedelta(seconds=ttl + 1)),
